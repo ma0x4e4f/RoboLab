21878541a810f463f47acfdc03e4f7b32fbbd8d0
Manojkumar <manojcrisdo@live.com>
2014-11-08 20:12:29 +0100

    Count turns

--- src/main.c
+++ src/main.c
@@ -30,21 +30,37 @@ TASK(OSEK_Main_Task) {
 
 		int RN = 50;
 
+		int currentRev = ecrobot_get_motor_rev(NXT_PORT_B);
+		int oldRev = ecrobot_get_motor_rev(NXT_PORT_B);
+
+
+//		nxt_motor_set_count(NXT_PORT_B, 180);
+//		nxt_motor_set_count(NXT_PORT_C, 90);
+
 		nxt_motor_set_speed(NXT_PORT_B, RN, 1); 			/* Set motor speed for B and C to RN */
 		nxt_motor_set_speed(NXT_PORT_C, RN, 1);
 
+
 		/* 500msec wait */
-		systick_wait_ms(5000);
+//		systick_wait_ms(5000);
+
+		while(n < 180)
+		{
+			currentRev = ecrobot_get_motor_rev(NXT_PORT_B);
+			n = currentRev-oldRev;
+		}
+
+		oldRev = currentRev;
 
 		nxt_motor_set_speed(NXT_PORT_B, 0, 1);
 		nxt_motor_set_speed(NXT_PORT_C, 0, 1);
 
-		systick_wait_ms(2000);
+//		systick_wait_ms(2000);
+//
+//		nxt_motor_set_speed(NXT_PORT_B, 50, 1);
+//		nxt_motor_set_speed(NXT_PORT_C, -50, 1);
 
-		nxt_motor_set_speed(NXT_PORT_B, 50, 1);
-		nxt_motor_set_speed(NXT_PORT_C, -50, 1);
-
-		systick_wait_ms(5000);
+		systick_wait_ms(15000);
 
 		nxt_motor_set_speed(NXT_PORT_B, 0, 1);
 		nxt_motor_set_speed(NXT_PORT_C, 0, 1);
